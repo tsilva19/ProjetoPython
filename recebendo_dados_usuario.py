08ef8bd7bd3817113d2f92b98966285d49fff8c0
Thiago <thiagoalves060@gmail.com>
2021-03-28 11:49:59 -0300

    terceiro commite

--- recebendo_dados_usuario.py
+++ recebendo_dados_usuario.py
@@ -1,6 +1,23 @@
 """
 Recebendo dados do usuario
+
+input() -> Todo dado recebido  via input é do tipo String
+
+Em Python , string é tudo que estiver entre:
+
+- Aspas simples;
+- Aspas duplas;
+- Aspas simples triplas;
+- Aspas duplas triplas;
+
+Exemplos:
+- Aspas simples -> 'Angelina Jolie'
+- Aspas duplas -> "Angelina Jolie"
+- Aspas simples triplas -> '''Angelina Jolie'''
+- Aspas duplas triplas ->
 """
+
+
 # //Entrada de Dados
 print("Qual o seu nome ? ")
 nome = input()  # Input -> Entrada
@@ -11,7 +28,11 @@ idade = input()
 # Processamento
 
 
-# Saida de dados
-print('Seja bem-vindo(a) %s' % nome)
+# Saida de dados 2.x
+# print('Seja bem-vindo(a) %s' % nome)
 
-print('A %s tem %s anos' % (nome, idade))
+# Exemplo de print 'moderno' 3.x
+# print('Seja bem-vindo(a) %s' % nome)
+
+# Exemplo de print 'moderno' 3.x
+# print('A %s tem %s anos' % (nome, idade))
---
+++ tipo_numerico.py
@@ -0,0 +1,53 @@
+"""
+Tipo Numerico
+"""
+# Subtração
+var = 7 - 2
+
+print("Subtração:")
+print(var)
+
+# Multiplicação
+var = 3 * 5
+print("Multiplicação:")
+print(var)
+
+# Divisão
+var = 5 / 2
+print("Divisão:")
+print(var)
+
+# Retorna Inteiro da Divisão
+var = int(5 / 2)
+print("Retorna Inteiro da Divisão:")
+print(var)
+
+# Retorna Inteiro da Divisão
+var = 5 // 2
+print("Retorna Inteiro da Divisão:")
+print(var)
+
+# Resto da Divisão
+var = 4 % 2
+print("Resto da Divisão:")
+print(var)
+
+# Resto da Divisão
+var = 5 % 2
+print("Resto da Divisão:")
+print(var)
+
+# 3 ** 3 3 elevado a 3
+var = 3 ** 3
+print("3 Elevado a 3:")
+print(var)
+
+# 2 ** 8 2 elevado a 8
+var = 2 ** 8
+print("2 Elevado a 8:")
+print(var)
+
+# 2 ** 63 2 elevado a 63
+var = 2 ** 63
+print("2 Elevado a 63:")
+print(var)
