edda8e52efc8a5f6676ea84b5498d02e09e71435
Thiago <thiagoalves060@gmail.com>
2021-03-28 11:14:32 -0300

    Segundo commite

---
+++ recebendo_dados_usuario.py
@@ -0,0 +1,17 @@
+"""
+Recebendo dados do usuario
+"""
+# //Entrada de Dados
+print("Qual o seu nome ? ")
+nome = input()  # Input -> Entrada
+
+print("Qual a sua idade? ")
+idade = input()
+
+# Processamento
+
+
+# Saida de dados
+print('Seja bem-vindo(a) %s' % nome)
+
+print('A %s tem %s anos' % (nome, idade))
